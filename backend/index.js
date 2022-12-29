5b070106b1f0efffea340ab6a5cb6efbd951914a
jehianth <jehianbusiness.com>
2022-12-29 09:40:00 +0700

    finish backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,24 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import UserRoute from "./routes/UserRoute.js";
 
 const app = express();
+
+mongoose.set('strictQuery',false);
+mongoose.connect('mongodb://127.0.0.1/crud_db',{
+    useNewUrlParser: true,
+    UseUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log('Database connected...'));
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(UserRoute);
+
 app.listen(5000, ()=> console.log('Server up and running...'));
